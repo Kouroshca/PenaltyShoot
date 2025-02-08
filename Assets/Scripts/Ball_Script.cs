4ab8ff7ea5ae67e37851c007cd8eac26a0ad9a27
Kouroshca <156234772+Kouroshca@users.noreply.github.com>
2025-02-08 16:06:19 -0500

    adding slider and adding new assest for the ball and the goal

--- Assets/Scripts/Ball_Script.cs
+++ Assets/Scripts/Ball_Script.cs
@@ -2,26 +2,55 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball_Script : MonoBehaviour
 {
     public float Force;
     public Transform Target;
-
+    public Slider forceUI;
+ 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space)) // it will fill slider dependin on pressure
+        {
+            Force++; // hold the button to add force to the ball
+            Slider();
+        }
+        
+        
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             Shoot();
-            Destroy(gameObject, 5f);
+            StartCoroutine(Wait());
 
         }
+
+        
     }
+
 
     private void Shoot()
     {
         Vector3 ShootForce = (Target.position - this.transform.position).normalized;
-        
-        GetComponent<Rigidbody>().AddForce(ShootForce * Force + new Vector3(0, 3f, 0), ForceMode.Impulse);
+        Debug.Log("Shoot");
+       // GetComponent<Rigidbody>().AddForce(ShootForce * Force + new Vector3(0, 3f, 0), ForceMode.Impulse);
+        GetComponent<Rigidbody>().AddForce(ShootForce * Force, ForceMode.Impulse);
+    }
+    private void Slider()
+    {
+        forceUI.value = Force;
+    }
+
+    public void ResetGauge() 
+    {
+        Force = 0;
+        forceUI.value = 0;
+    }
+
+    IEnumerator Wait() 
+    {
+        yield return new WaitForSeconds(1.5f);
+        ResetGauge();
     }
 }
